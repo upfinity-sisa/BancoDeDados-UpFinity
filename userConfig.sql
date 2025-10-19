d3c38087bfd8957d05f01d32b23b13b156b1e098
OmarDahbur <omar.dahbur@sptech.school>
2025-10-19 13:46:38 -0300

    add: usuario upfinity

--- userConfig.sql
+++ userConfig.sql
@@ -1,15 +1,4 @@
-DROP USER IF EXISTS upfinity_select;
-CREATE USER 'upfinity_select'@'%' IDENTIFIED BY 'Urubu100$';
-GRANT ALL PRIVILEGES ON db.* TO 'upfinity_select'@'%';
-FLUSH PRIVILEGES;
-
-DROP USER IF EXISTS upfinity_crud;
-CREATE USER 'upfinity_crud'@'%' IDENTIFIED BY 'Urubu100$';
-GRANT ALL PRIVILEGES ON db.* TO 'upfinity_crud'@'%';
-FLUSH PRIVILEGES;
-
-DROP USER IF EXISTS dev;
-CREATE USER 'dev'@'%' IDENTIFIED BY 'Urubu100$';
-GRANT SELECT ON db.* TO 'dev'@'%';
-
+DROP USER IF EXISTS funcUpFinity;
+CREATE USER 'funcUpFinity'@'%' IDENTIFIED BY 'Upfinity123';
+GRANT ALL PRIVILEGES ON db.* TO 'funcUpFinity'@'%';
 FLUSH PRIVILEGES;
