817dae7040eac279cdfc777adbef15e74fa05613
CatarinaGimenes <catarina.pires@sptech.school>
2025-11-16 22:26:56 -0300

    Add: Adicionando meus scripts + atualização na modelagem para o meu individual

---
+++ scripts/individualCATARINA/insertsSeguranca.sql
@@ -0,0 +1,7 @@
+use upfinity;
+
+insert into Seguranca values
+(1, 'arquivo', 0, 1),
+(2, 'conexao', 0, 1),
+(3, 'invasao', null, 1),
+(4, 'processo', 0, 1);
---
+++ scripts/individualCATARINA/tabelasSeguranca.sql
@@ -0,0 +1,77 @@
+#CATARINA TABLES
+
+use upfinity;
+
+CREATE TABLE IF NOT EXISTS Seguranca (
+idSeguranca INT PRIMARY KEY,
+categoria VARCHAR(45),
+salvamento BOOLEAN,
+fkAtm INT,
+FOREIGN KEY (fkAtm) references Atm(idAtm),
+CHECK (categoria in('arquivo', 'conexao', 'invasao', 'processo'))
+);
+
+CREATE TABLE IF NOT EXISTS ItemSalvo (
+idItemSalvo INT AUTO_INCREMENT,
+categoria VARCHAR(45),
+conteudo01 VARCHAR(200),
+conteudo02 VARCHAR(200),
+fkSeguranca INT,
+PRIMARY KEY (idItemSalvo, fkSeguranca),
+FOREIGN KEY (fkSeguranca) REFERENCES Seguranca(idSeguranca),
+CHECK (categoria in('arquivo', 'conexao', 'processo'))
+);
+
+CREATE TABLE IF NOT EXISTS AlertaSeguranca (
+idAlertaSeguranca INT PRIMARY KEY AUTO_INCREMENT,
+categoria VARCHAR(45),
+mensagem VARCHAR(60),
+horario DATETIME,
+CHECK (categoria in('arquivo', 'conexao', 'invasao', 'processo'))
+);
+
+CREATE TABLE IF NOT EXISTS Invasao (
+idInvasao INT AUTO_INCREMENT,
+horario DATETIME,
+IP CHAR(15),
+fkSeguranca INT,
+fkAlertaSeguranca INT,
+PRIMARY KEY (idInvasao, fkSeguranca, fkAlertaSeguranca),
+FOREIGN KEY (fkSeguranca) REFERENCES Seguranca(idSeguranca),
+FOREIGN KEY (fkAlertaSeguranca) REFERENCES AlertaSeguranca(idAlertaSeguranca)
+);
+
+CREATE TABLE IF NOT EXISTS ConexaoAberta (
+idConexaoAberta INT AUTO_INCREMENT,
+portaLocal VARCHAR(45),
+IPremoto VARCHAR(45),
+horario DATETIME,
+fkSeguranca INT,
+fkAlertaSeguranca INT,
+PRIMARY KEY (idConexaoAberta, fkSeguranca),
+FOREIGN KEY (fkSeguranca) REFERENCES Seguranca(idSeguranca),
+FOREIGN KEY (fkAlertaSeguranca) REFERENCES AlertaSeguranca(idAlertaSeguranca)
+);
+
+CREATE TABLE IF NOT EXISTS ProcessoPrivilegiado (
+idProcessoPrivilegiado INT AUTO_INCREMENT,
+nome VARCHAR(60),
+horario DATETIME,
+fkSeguranca INT, 
+fkAlertaSeguranca INT,
+PRIMARY KEY (idProcessoPrivilegiado, fkSeguranca),
+FOREIGN KEY (fkSeguranca) REFERENCES Seguranca(idSeguranca),
+FOREIGN KEY (fkAlertaSeguranca) REFERENCES AlertaSeguranca(idAlertaSeguranca)
+);
+
+CREATE TABLE IF NOT EXISTS ArquivoCritico (
+idArquivoCritico INT AUTO_INCREMENT,
+nome VARCHAR(60),
+hashArqCritico VARCHAR(200),
+horario DATETIME,
+fkSeguranca INT, 
+fkAlertaSeguranca INT,
+PRIMARY KEY (idArquivoCritico, fkSeguranca),
+FOREIGN KEY (fkSeguranca) REFERENCES Seguranca(idSeguranca),
+FOREIGN KEY (fkAlertaSeguranca) REFERENCES AlertaSeguranca(idAlertaSeguranca)
+);
