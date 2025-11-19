5493dd43b46b069196b5d626f4f2c024c05db588
CatarinaGimenes <catarina.pires@sptech.school>
2025-11-19 18:44:25 -0300

    Update: Atualizando tabela da empresa para adicionar o id do Slack

--- scripts/dbStructure_upfinity_V2.sql
+++ scripts/dbStructure_upfinity_V2.sql
@@ -47,6 +47,7 @@ CREATE TABLE IF NOT EXISTS Empresa (
     statusAprovacao INT NOT NULL DEFAULT 0,
     statusPagamento BOOLEAN NOT NULL DEFAULT FALSE,
     nomeFantasia VARCHAR(45),
+    idSlack INT UNIQUE,
     FOREIGN KEY (fkPlano) REFERENCES Plano(idPlano),
     FOREIGN KEY (fkEndereco) REFERENCES Endereco(idEndereco)
 );
