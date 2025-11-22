5b38e3d7a744f364565c429ab2406ca756357a5a
CatarinaGimenes <catarina.pires@sptech.school>
2025-11-22 00:00:01 -0300

    Fix: Ajustando mocks

--- scripts/individualCATARINA/insertsSeguranca.sql
+++ scripts/individualCATARINA/insertsSeguranca.sql
@@ -3,7 +3,7 @@ use upfinity;
 insert into Seguranca values
 (1, 'arquivo', 0, 1),
 (2, 'conexao', 0, 1),
-(3, 'arquivo', null, 1);
+(3, 'invasao', null, 1);
 
 #semana 1
 insert into AlertaSeguranca values
